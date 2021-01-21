78427b01507ac1411c5dd64e1c8e3632d3fcf153
t-o-m-spider <77025964+t-o-m-spider@users.noreply.github.com>
2021-01-21 21:21:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,24 @@ $(function() {
 	$('#sub-title').fadeIn(2000);
 
 	$('#About').fadeIn(2000);
+
+	$('#Home-scroll').click(function() {
+		$("html,body").animate({scrollTop:0
+		},'500');
+	});
+
+	$('#About-scroll').click(function() {
+		$("html,body").animate({scrollTop:$('#About').offset().top -100
+		},'500');
+	});
+
+	$('#Offer-scroll').click(function() {
+		$("html,body").animate({scrollTop:$('#Offer').offset().top -100
+		},'500');
+	});
+
+	$('#Contact-scroll').click(function() {
+		$("html,body").animate({scrollTop:$('#Contact').offset().top -100
+		},'500');
+	});
 });
