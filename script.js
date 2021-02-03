f66b1c50b31c86c4924a210c4214ef0b0b014cd7
t-o-m-spider <77025964+t-o-m-spider@users.noreply.github.com>
2021-02-03 11:46:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
+// JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+
+	document.getElementById('MenuButton').addEventListener("click", function () {
+
+		this.classList.toggle("active");
+
+		document.getElementById("Spmenu").classList.toggle("active");
+
+		document.getElementById("Spmenu-container").classList.toggle("active");
+	})
+
+	document.getElementById("Spmenu-container").addEventListener("click", function() {
+
+  		this.classList.toggle("active");
+
+  		document.getElementById("Spmenu").classList.toggle("active");
+
+  		document.getElementById("MenuButton").classList.toggle("active");
+
+	})
+});
+
+// jQuery
 $(function() {
+
 	$('#main-title').fadeIn(2000);
 
 	$('#sub-title').fadeIn(2000);
 
 	$('#About').fadeIn(2000);
 
-	$('#Home-scroll').click(function() {
+	$('.Home-scroll').click(function() {
 		$("html,body").animate({scrollTop:0
-		},'500');
+		},'1000');
 	});
 
-	$('#About-scroll').click(function() {
+	$('.About-scroll').click(function() {
 		$("html,body").animate({scrollTop:$('#About').offset().top -100
-		},'500');
+		},'1000');
 	});
 
-	$('#Offer-scroll').click(function() {
+	$('.Offer-scroll').click(function() {
 		$("html,body").animate({scrollTop:$('#Offer').offset().top -100
-		},'500');
+		},'1000');
 	});
 
-	$('#Contact-scroll').click(function() {
+	$('.Contact-scroll').click(function() {
 		$("html,body").animate({scrollTop:$('#Contact').offset().top -100
-		},'500');
+		},'1000');
 	});
 });
