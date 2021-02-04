a9486be3f587161234a326b69bd937174ad56ee7
t-o-m-spider <77025964+t-o-m-spider@users.noreply.github.com>
2021-02-05 07:08:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -19,16 +19,27 @@ document.addEventListener('DOMContentLoaded', function() {
   		document.getElementById("MenuButton").classList.toggle("active");
 
 	})
+
+	// セレクタを取り込む
+	const targetElement = document.querySelectorAll(".animationTarget");
+	console.log("画面の高さ", window.innerHeight)
+	// スクロールした時、常にtargetElementの高さを取得する
+	document.addEventListener("scroll", function() {
+		for (let i = 0; i < targetElement.length; i++) {
+			const getElementDistance = targetElement[i].
+			// liの全体の高さの0.6の高さまでスクロールしたらshowクラスをつける
+			getBoundingClientRect().top +targetElement[i].clientHeight * .7
+			// 現在のブラウザの高さを取得する
+			if(window.innerHeight > getElementDistance) {
+				// i番目のliが見えた時点でshowというクラスをつける
+				targetElement[i].classList.add("show");
+			}
+		}
+	})
 });
 
 // jQuery
 $(function() {
-
-	$('#main-title').fadeIn(2000);
-
-	$('#sub-title').fadeIn(2000);
-
-	$('#About').fadeIn(2000);
 
 	$('.Home-scroll').click(function() {
 		$("html,body").animate({scrollTop:0
